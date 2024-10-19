fd42452c688a7ce8810a0f58962af0b67bcda51b
Pedro Henrique Pimentel da Silva <ph024011@gmail.com>
2024-10-19 17:42:39 -0300

    Adicionando o Começo do código do UVV Fintech

---
+++ UVV_Fintech_Project/Model/Cliente.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UVV_Fintech_Project.Model
+{
+    internal class Cliente
+    {
+        private readonly int id;
+        public readonly string nome;
+        public readonly string sobrenome;
+        public readonly string cpf;
+        int anoNascimento;
+        int diaNascimento;
+        int mesNascimento;
+
+        private string nomeCompleto;
+
+        DateTime nascimento;
+
+        public Cliente(string nome, string sobrenome, string cpf, int anoNascimento, int mesNascimento, int diaNascimento)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.cpf = cpf;
+
+            nomeCompleto = nome + sobrenome;
+
+            this.anoNascimento = anoNascimento;
+            this.mesNascimento = mesNascimento;
+            this.diaNascimento = diaNascimento;
+
+            nascimento = new DateTime(anoNascimento, mesNascimento, diaNascimento);
+        }
+
+        public int CalcularIdade(DateTime dataNacimento)
+        {
+            DateTime dataDeHoje = DateTime.Today;
+            int idade = dataDeHoje.Year - dataNacimento.Year;
+
+            if (dataNacimento.Date > dataDeHoje.AddYears(-idade))
+            {
+                idade--;
+            }
+
+            return idade;
+        }
+
+
+
+        
+    }
+}
---
+++ UVV_Fintech_Project/Persistencia/BD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UVV_Fintech_Project.Model;
+
+namespace UVV_Fintech_Project.Persistencia
+{
+    internal class BD
+    {
+        // Persistência dos clientes <CPF, Nome
+        Dictionary<string, string> persistenciaClientes = new();
+
+        public void AdicionarCliente(string cpf, string nomeCompleto)
+        {
+            if (!persistenciaClientes.ContainsKey(cpf))
+            {
+                persistenciaClientes.Add(cpf, nomeCompleto);
+                Console.WriteLine("Cliente adicionado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("CPF já existe no sistema!");
+            }
+        }
+
+        // Método para acessar um cliente
+        public string ObterCliente(string cpf)
+        {
+            // Tenta buscar o cliente pelo CPF
+            if (persistenciaClientes.TryGetValue(cpf, out string nomeCompleto))
+            {
+                return $"Cliente encontrado: {nomeCompleto}";
+            }
+            else
+            {
+                return "Cliente não encontrado!";
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
